2193827c49458baccc2ccf38e82fab912f0132db
hukumran-san <71653875+hukumran-san@users.noreply.github.com>
2022-07-21 00:49:20 +0300

    v 0.0.6 commit
    calculate update in frontend

--- target/classes/static/script.js
+++ target/classes/static/script.js
@@ -21,17 +21,40 @@ function createListElement(id,item,listType)
 
 document.getElementById("currencyList1").addEventListener("click",function(e){
     if(e.target && e.target.classList.contains("dropdown-item"))
+    {
         var splitData    = e.target.innerText.split("-");
         var currencyCode = splitData[0];
         var currencyRate  = currencyList.filter(item=>item.CurrencyCode==currencyCode).map(item=>item.BanknoteBuying)[0];
         document.getElementById("currencyInput1").value = currencyRate;
+        var currencyName  = currencyList.filter(item=>item.CurrencyCode==currencyCode).map(item=>item.CurrencyName)[0];
+        document.getElementById("btnCurrencyCode1").innerText = currencyName;
+    }
+})
 
+document.getElementById("btnCalculate").addEventListener("click",function(e)
+{
+    var currencyInput3 = document.getElementById("currencyInput3").value
+    if(currencyInput3 != undefined && 
+        currencyInput3 != "" &&
+        document.getElementById("btnCurrencyCode1").innerText.trim() != "Döviz Cinsi-1")
+    {
+        if(document.getElementById("btnCurrencyCode2").innerText.trim() != "Döviz Cinsi-2")
+        {
+            var result = document.getElementById("currencyInput1").value * currencyInput3;
+            result = result/document.getElementById("currencyInput2").value
+            document.getElementById("currencyInput4").value = result;
+        }
+    }
 })
 
 document.getElementById("currencyList2").addEventListener("click",function(e){
     if(e.target && e.target.classList.contains("dropdown-item"))
+    {
         var splitData = e.target.innerText.split("-");
         var currencyCode = splitData[0];
         var currencyRate  = currencyList.filter(item=>item.CurrencyCode==currencyCode).map(item=>item.BanknoteBuying)[0];
         document.getElementById("currencyInput2").value = currencyRate;
+        var currencyName  = currencyList.filter(item=>item.CurrencyCode==currencyCode).map(item=>item.CurrencyName)[0];
+        document.getElementById("btnCurrencyCode2").innerText = currencyName;
+    }
 })
